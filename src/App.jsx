63c46a2984080308053989b07303cbaf7768fb1b
Milan960 <milan.sachani.ms@gmail.com>
2022-07-24 11:33:32 +0200

    footer has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Suspense } from "react";
 import { Box } from "@mui/material";
 
-import BaseRoutes from "Routes/Routes";
+import BaseRoutes from "Routes/BaseRoutes";
+import Navbar from "Components/Navbar/Navbar";
+import Footer from "Components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Box width="400px">
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
         <Suspense>
-          <Router>
-            <BaseRoutes />
-          </Router>
+          <Navbar />
+          <BaseRoutes />
+          <Footer />
         </Suspense>
       </Box>
     </>
