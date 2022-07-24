c5396cb86b17fce4f3a22ab507d32b6c6faaeb4c
Milan960 <milan.sachani.ms@gmail.com>
2022-07-24 12:34:23 +0200

    Exercises pages  has been added 🔥

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,27 @@ import React, { useState } from "react";
 import { Box } from "@mui/material";
 
 import HeroBanner from "Components/HeroBanner/HeroBanner";
+import SearchExercises from "Components/SearchExercises/SearchExercises";
+import Exercises from "Components/Exercises/Exercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
   return (
     <>
-      <HeroBanner />
-      {/* <h1>Home</h1> */}
+      <Box>
+        <HeroBanner />
+        <SearchExercises
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
+        <Exercises
+          setExercises={setExercises}
+          exercises={exercises}
+          bodyPart={bodyPart}
+        />
+      </Box>
     </>
   );
 };
