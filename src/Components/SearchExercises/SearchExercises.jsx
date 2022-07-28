d11b8f40333e2f6c068396308d13bdb66f349d70
Milan960 <milan.sachani.ms@gmail.com>
2022-07-28 13:31:04 +0200

    Exercises pages  has been added 🔥

--- src/Components/SearchExercises/SearchExercises.jsx
+++ src/Components/SearchExercises/SearchExercises.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+
+  return (
+    <>
+      <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+        <Typography
+          fontWeight={700}
+          sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+          mb="49px"
+          textAlign="center"
+        >
+          Awesome Exercises You <br /> Should Know
+        </Typography>
+       
+      </Stack>
+    </>
+  );
 };
 
 export default SearchExercises;
