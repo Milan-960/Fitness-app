c9722c77c863e10f04d2147055d6d37beb14568e
Milan960 <milan.sachani.ms@gmail.com>
2022-07-30 08:19:57 +0200

    develop branch has been added with i18

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,10 @@ import BaseRoutes from "Routes/BaseRoutes";
 import Navbar from "Components/Navbar/Navbar";
 import Footer from "Components/Footer/Footer";
 
-import { useTranslation } from "react-i18next";
 
 import "./App.css";
 
 function App() {
-
-   const { i18n } = useTranslation();
-
-   const changeLanguageHandler = (lang) => {
-      i18n.changeLanguage("de");
-   };
-
 
   return (
     <>
