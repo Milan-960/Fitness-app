f4d1bb87d9167e659e3cc8be3272ecb1a5c48329
Milan960 <milan.sachani.ms@gmail.com>
2022-07-30 08:15:53 +0200

    reacti18 has been added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,19 @@ import BaseRoutes from "Routes/BaseRoutes";
 import Navbar from "Components/Navbar/Navbar";
 import Footer from "Components/Footer/Footer";
 
+import { useTranslation } from "react-i18next";
+
 import "./App.css";
 
 function App() {
+
+   const { i18n } = useTranslation();
+
+   const changeLanguageHandler = (lang) => {
+      i18n.changeLanguage("de");
+   };
+
+
   return (
     <>
       <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,18 @@ import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 import Logo from 'assets/images/Logo.png';
+import { useTranslation } from "react-i18next";
 
-const Navbar = () => (
+const Navbar = () => {
+  
+   const { i18n } = useTranslation();
+
+    const changeLanguageHandler = (lang) => {
+        i18n.changeLanguage(lang);
+    }
+  
+return(
+  <>
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
       <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
@@ -18,8 +28,17 @@ const Navbar = () => (
     >
       <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
       <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+       <select
+            value={i18n.language}
+            onChange={changeLanguageHandler}
+          >
+            <option value="en">English</option>
+            <option value="sp">Spanish</option>
+        </select>
     </Stack>
   </Stack>
-);
+  </>
+
+)}
 
 export default Navbar;
