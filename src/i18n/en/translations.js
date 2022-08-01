8cda557feabb13b603d50a0ac7f074868fc8c846
Milan960 <milan.sachani.ms@gmail.com>
2022-08-01 21:34:59 +0530

    land has been added 🔥

--- src/i18n/en/translations.js
+++ src/i18n/en/translations.js
@@ -1,4 +1,3 @@
 export const TRANSLATIONS_EN = {
-  welcome: "welcome to the",
   Exercises: "Awesome Exercises You Should Know",
 };
--- src/index.js
+++ src/index.js
@@ -6,21 +6,12 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-import { I18nextProvider } from "react-i18next";
-import i18next from "i18next";
-
-i18next.init({
-  interpolation: { escapeValue: false }, // React already does escaping
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </I18nextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
