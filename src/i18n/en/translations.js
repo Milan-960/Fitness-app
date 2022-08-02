abea965113bfc9a579f075af9d9248a28ee3df3f
Milan960 <milan.sachani.ms@gmail.com>
2022-08-02 09:12:27 +0530

    lang has been added 🔥

--- src/i18n/en/translations.js
+++ src/i18n/en/translations.js
@@ -1,4 +1,6 @@
 export const TRANSLATIONS_EN = {
   Home: "Home",
   Exercises: "Awesome Exercises You Should Know",
+  HomePageSlogan: "Sweat, Smile And Repeat",
+  Homepage: "Check out the most effective exercises personalized to you",
 };
--- src/i18n/sp/translation.js
+++ src/i18n/sp/translation.js
@@ -1,4 +1,5 @@
 export const TRANSLATIONS_SP = {
   Home: "Home",
   Exercises: "Ejercicios impresionantes que debes saber",
+  Homepage: "Checa los ejercicios más efectivos personalizados para ti",
 };
