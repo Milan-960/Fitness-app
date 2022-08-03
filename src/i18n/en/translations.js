1a5604d6df0d0ca1aadb0aa30826e56dea5f7ce8
Milan960 <milan.sachani.ms@gmail.com>
2022-08-03 09:39:57 +0530

    404 i18 added

--- src/i18n/en/translations.js
+++ src/i18n/en/translations.js
@@ -1,5 +1,4 @@
 export const TRANSLATIONS_EN = {
-  Home: "Home",
   Exercises: "Awesome Exercises You Should Know",
   HomePageSlogan: "Sweat, Smile And Repeat",
   Homepage: "Check out the most effective exercises personalized to you",
