a75695d097838f0a81bdbab5ca61611532617f08
Milan960 <milan.sachani.ms@gmail.com>
2022-08-03 09:35:32 +0530

    404 i18 added

--- src/i18n/en/translations.js
+++ src/i18n/en/translations.js
@@ -3,4 +3,7 @@ export const TRANSLATIONS_EN = {
   Exercises: "Awesome Exercises You Should Know",
   HomePageSlogan: "Sweat, Smile And Repeat",
   Homepage: "Check out the most effective exercises personalized to you",
+
+  //404 error
+  NotFound: "Page Not Found!!",
 };
